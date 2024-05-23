033d1415a3e287d0fccc52a3ba485811ec887f2f
gretmn102 <erlgreywatson@gmail.com>
2024-05-23 05:44:21 +0300

    feat: add Docusaurus

---
+++ docusaurus.config.ts
@@ -0,0 +1,136 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import remarkBreaks from 'remark-breaks';
+
+const discordInviteUrl = 'https://discord.gg/eqnEu2jzxe';
+
+const config: Config = {
+  title: 'Tournament',
+  tagline: 'Выбери победителя!',
+  favicon: 'img/favicon.png',
+  customFields: {
+    discordInviteUrl: discordInviteUrl,
+  },
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'funnysockorg', // Usually your GitHub org/user name.
+  projectName: 'funnysockorg.github.io', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/funnysockorg/Tournament.Docs/edit/main/',
+          remarkPlugins: [remarkBreaks],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/funnysockorg/Tournament.Docs/edit/main/',
+          remarkPlugins: [remarkBreaks],
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themes: ['@docusaurus/theme-mermaid'],
+
+  markdown: {
+    mermaid: true,
+  },
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/funnysock-social-card.png',
+    navbar: {
+      title: 'Tournament',
+      logo: {
+        alt: 'Tournament logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/funnysockorg',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Документация',
+          items: [
+            {
+              label: 'Docs',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Сообщество',
+          items: [
+            {
+              label: 'Discord',
+              href: discordInviteUrl,
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/funnysockorg',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} FunnySock Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+    colorMode: {
+      respectPrefersColorScheme: true,
+    }
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
